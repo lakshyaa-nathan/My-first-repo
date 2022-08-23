a64af93a28de1babbfbb1c04774cd73553b0be10
lakshyaa-nathan <111480891+lakshyaa-nathan@users.noreply.github.com>
2022-08-23 12:38:58 -0500

    Started BlueJ and chapter 1 notes

---
+++ My first project/chapter1Notes.java
@@ -0,0 +1,49 @@
+
+/**
+ * lakshyaa Nathan
+ * 8/23/22
+ * Notes for chapter 1 
+ * Write a description where we are going to take some code notes
+ * Who helped me: Mr. Hayes, Mom
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class chapter1Notes
+{
+
+    /**
+     * main method
+     * where are we going to code
+     */
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        
+        //these are the basic data types, mathematical (add, subtract, etc. )
+        int x=9, y, z;
+        
+        //takes up 8 bytes
+        double num = 5, num1 = 23.457, num2;
+        
+        //single letter
+        char letter = 'd';
+        
+        // true or false
+        boolean choice = false;
+        
+        // takes up for 4 bytes
+        float anum = 7;
+        
+        // integers - need to take up more memory
+        long a = 9;
+        short b = 5;
+        
+        //These are objects, series of letters
+        String word = new String("Kevin Hayes");
+        // doubles and integers are objects that have funtionality
+        Integer number = new Integer(47);
+        Double number2 = new Double(34.5);
+    }
+}
